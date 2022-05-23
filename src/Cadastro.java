1e5fba3b9784d8aa9e6cdb55b8dc28c5fe66ec98
Marcus Filipe Quirino <marcus@Marcuss-MacBook-Air-2.local>
2022-05-23 18:13:52 -0300

    classes criadas. lista de afazeres

---
+++ src/Cadastro.java
@@ -0,0 +1,12 @@
+public class Cadastro {
+    //iniciar 5 funcionarios 
+
+    //menu de opcoes
+    //cadastrar funcionario
+    //listar funcionarios cadastrados
+    //remover um funcionario a partir do cpf
+    //funcionario com maior salarios
+    //editar o salario pelo cpf
+    //porcentagem de funcionarios que possuem filhos
+    //sair 
+}
---
+++ src/Funcionarios.java
@@ -0,0 +1,10 @@
+public class Funcionarios{
+    //funcionario: nome, cpf, valor hora trabalhada, hora trabalhada (max. 44h), possui filhos 
+
+    //construtores, getters, setters, toString
+
+    //calculo de salario de funcionario
+
+    
+
+}
